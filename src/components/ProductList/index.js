1b480863a570510fc1086b30cb0f1b519f808a04
rohita.chapter247@gmail.com <you@example.com>
2019-01-21 11:24:35 +0530

    slider

--- src/components/ProductList/index.js
+++ src/components/ProductList/index.js
@@ -1,15 +1,64 @@
 import React from 'react';
 import Constant from '../../Config';
-import ReactTooltip from 'react-tooltip'
+import MappleToolTip  from 'reactjs-mappletooltip'
 const Productlist =(props)=>{ 
     const image=props.product.photo_thumbnail.split(',');
-    return(
+    let active='';
+    let currentImage=image[0];
+
+    if(props.currentActive===props.product.incredible_id){
+        active='active';
+        if(props.viewImage!=='')
+            currentImage=props.viewImage;
+
+    }
     
-        <div className="col-lg-3 col-md-4 col-xs-6" >
-            <a href="#" className="d-block mb-4 h-100">
-                <img className="img-fluid img-thumbnail" src={Constant.img_url+image[0]} alt={props.product.name} />
-                <h4>{props.product.name}{" "}({image.length})</h4>
-            </a>
+    return(
+        
+        <div className={"col-lg-3 col-md-3 col-xs-6 resultitem "+active}  >
+           <div className="box thumbnailurl"> 
+                <a href="javascript:void(0)" className="d-block mb-4 h-100" onClick={props.handleClick} ref={props.myRef}>
+                    <img className="img-fluid img-thumbnail" src={Constant.img_url+image[0]} alt={props.product.name}  />
+                    <MappleToolTip float={true} direction={'bottom'} mappleType={'success'} shadow={true} fadeInAnimation={true}>
+                    <h4>{props.product.name}{" "}({image.length})</h4><div>{props.product.name}{" "}({image.length})</div></MappleToolTip>
+                </a>
+            </div>
+                <div className="main-silder">
+
+                    <div className="single-img-warp flexbox-container">
+                        
+                        <div className="col-sm-6">
+                            
+                            <div className="col-sm-12">
+                            {(props.index!==0)? (
+                            <a className="move-arrow left detailPrev" href="javascript:;" onClick={props.prevImage}>
+                                <img alt="Icon_Basic_Left.svg" className="" width="25" src={Constant.img_url+'assets/img/icons/left-arrow.svg'} />
+                            </a>) : null }
+                            <img className="img-fluid img-div-main img-thumbnail" src={Constant.img_url+currentImage} alt={props.product.name} /></div>
+                        </div>
+                        <div className="col-sm-6 small">
+                            <div className="col-sm-12"> 
+                                <a className="detailClose" href="javascript:void(0)" onClick={props.removeClass}>
+                                    <img alt="Icon_Basic_Close.svg" width="20" src={Constant.img_url+'assets/img/close.svg'}></img>
+                                </a>
+                                <h4>{props.product.name}{" "}({image.length})</h4>
+                                <div className="small-div">
+                                {image.map((img,index)=>(
+                                    <img className={"img-fluid img-thumbnail "+((currentImage===img) ? 'activeTham': '') } key={index} src={Constant.img_url+img} alt={props.product.name} onClick={()=>{props.changeImage(img)}} />
+                                ))}
+                                </div> 
+                                {(props.index!==(props.itemLegth-1)) ? (
+                                <a className="move-arrow right detailNext" href="javascript:;" onClick={props.nextImage}>
+                                    <img alt="Icon_Basic_Right.svg" className="" width="25" src={Constant.img_url+'assets/img/icons/right-arrow.svg'} />
+                                </a>) : null }  
+                            </div>
+                            
+                        </div>
+                        
+                    </div>
+                    
+                </div>
+            
         </div> 
      )};
 export default Productlist;
--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -8,28 +8,95 @@ class Home extends Component {
         super(props);
         this.state = {
             products:[],
+            viewDetails:false,
+            currentActive:0,
+            viewImage:'',
+            intervalId: 0,
+            scrollStepInPx:50,
+            delayInMs:16.66,
         }
-
+        this.divRef = React.createRef();
+        this.scrollStep=this.scrollStep.bind(this);
+        this.myRef = React.createRef();
+    }
+    scrollStep() {
+        console.log('ddssd');
+        console.log(window.pageYOffset+'   '+this.state.scrollStepInPx);
+        if (window.pageYOffset === 0) {
+            clearInterval(this.state.intervalId);
+        }
+        console.log(window.pageYOffset+'   '+this.state.scrollStepInPx);
+        window.scroll(0, window.pageYOffset - this.state.scrollStepInPx);
+    }
+    scrollToTop() {
+        let intervalId = setInterval(this.scrollStep, this.state.delayInMs);
+        this.setState({ intervalId: intervalId });
     }
     componentDidMount()
     {
         axios.get(Constant.backend_api+'/home/?auth=ExpediaFlow')
         .then(response=>(
             this.setState({
-                products:response.data.favorite_incredible_places
+                products:response.data.favorite_incredible_places,
+                viewImage:'',
             })
         )).catch(error=>(
             console.log(error)
         ));
     }
+    handleClick=(e,index) =>{
+       // this.scrollStep(this);
+        
+        if(this.state.currentActive===index)
+            index=0;
+        this.setState({
+            currentActive:index,
+            viewImage:'',
+        })
+        // console.log(index);
+        // console.log(e.target);
+        // var node = ReactDOM.findDOMNode(e.target);
+        console.log(this.myRef.current)
+        console.log(this.myRef.current.offsetTop)
+        // console.log(node.clientHeight)
+        // console.log(node.offsetTop)
+        
+    }
+    removeClass=(index)=>{
+        this.setState({
+            currentActive:0,
+            viewImage:'',
+        })     
+    }
+    prevImage=(index)=>{
+        this.setState({
+            currentActive:index,
+            viewImage:'',
+        })
+    }
+    nextImage=(index)=>{
+        this.setState({
+            currentActive:index,
+            viewImage:'',
+        })
+    }
+    changeImage=(image)=>{
+        this.setState({
+            viewImage:image,
+        })
+    }
     render() {
-        const productlist = this.state.products.map(product => (
-               <Productlist product={product} key={product.incredible_id}/>
-            )
-        )
+        const productlist = this.state.products.map((product,index) => {
+            let next=((this.state.products.length-1)!==index && this.state.products[index+1]['incredible_id']) ? this.state.products[index+1]['incredible_id'] : 0;
+            let prev=(index!==0  && this.state.products[index-1]['incredible_id']) ? this.state.products[index-1]['incredible_id'] : 0;
+            
+           return (            
+               <Productlist product={product} key={product.incredible_id} handleClick={(e)=>{this.handleClick(e,product.incredible_id)}} currentActive={this.state.currentActive} removeClass={this.removeClass.bind(this)} itemLegth={this.state.products.length} index={index} nextImage={()=>{this.prevImage(next)}} prevImage={()=>{this.nextImage(prev)}} viewImage={this.state.viewImage} changeImage={this.changeImage} myRef={this.myRef}  />
+            ) }
+        );
         return (
             <>
-                <div className="row text-center text-lg-left">
+                <div className="row text-center text-lg-left pictureView viewchange picflex" >
                     {productlist}
                     
                 </div>
